e07bd4322a35f9db4e1dae8505057c654af7764d
Cal Henderson <cal@slack-corp.com>
2015-10-12 10:15:52 -0700

    split batch files into 32 and 64 bit versions added setting of working directory so you can "right click -> run as admin" on the batch scripts as-is

---
+++ jbcert32.bat
@@ -0,0 +1,2 @@
+cd %~dp0\binaries
+@jailbreak32.exe %WINDIR%\system32\mmc.exe %WINDIR%\system32\certmgr.msc -32
---
+++ jbcsp64.bat
@@ -0,0 +1,8 @@
+@rem Useful command line
+@rem "-px <Key Container> <Out File> -pri"
+
+@rem if the key container is in the user store use
+@rem "-px <Key Container> <Out File> -pri -pku"
+
+cd %~dp0\binaries
+@jailbreak64.exe %WINDIR%\Microsoft.NET\Framework\v2.0.50727\aspnet_regiis.exe -px %*
